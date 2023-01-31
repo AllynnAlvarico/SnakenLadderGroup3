2929982aeb7de6bac11767c03cb0e9cf0761d1d6
Esvenn <L_esvenn@yahoo.com>
2023-01-31 15:19:48 +0000

    i did dice

--- SnakenLadderGroup3/Form1.vb
+++ SnakenLadderGroup3/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'declarations
+        Dim rndNumber As Random
+        Dim intValue As Integer
 
+        'generate a new object
+        rndNumber = New Random
+
+        'get value between 1 and 6
+        intValue = rndNumber.Next(1, 7)
+
+        'display the value in the label
+
+
+        Console.WriteLine(intValue)
+
+
+
+    End Sub
 End Class
