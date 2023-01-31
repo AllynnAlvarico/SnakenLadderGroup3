1d0864c8bd7a4144155dcbe25596bda3875e5ed5
20220554 <20220554@DFEI-S10-59>
2023-01-31 14:35:08 +0000

    Add project files.

---
+++ SnakenLadderGroup3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SnakenLadderGroup3")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("DFEi")>
+<Assembly: AssemblyProduct("SnakenLadderGroup3")>
+<Assembly: AssemblyCopyright("Copyright © DFEi 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9b85d343-e2f7-46ab-ac16-989f49a83b91")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
