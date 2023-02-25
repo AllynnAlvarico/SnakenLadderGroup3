d79b55a8bbaebf3596c349a4ae84e2a09dd99919
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2023-02-25 21:31:17 +0000

    modified few lines

--- SnakenLadderGroup3/pickCharacters.vb
+++ SnakenLadderGroup3/pickCharacters.vb
@@ -17,6 +17,12 @@
             chosenCharacters(player)
         Else
             MessageBox.Show("No Character has been Choosen")
+            player1 = 0
+            player2 = 0
+            pickMario.Enabled = True
+            pickLuigi.Enabled = True
+            pickPeach.Enabled = True
+            pickYoshi.Enabled = True
         End If
     End Sub
     Public Sub chosenCharacters(chosen As Integer)
@@ -26,74 +32,48 @@
             'this will allow you to be able to call or show the value in the dictionary
         End If
     End Sub
-    Public Sub pickTurn()
-        If player1Turn = True Then
+    Public Sub pickTurn(PlayerTurn As Boolean, playerSelected As Integer)
+        If PlayerTurn = True Then
             If selectMario = True Then
-                player1 = 1
+                playerSelected = 1
+                pickMario.Enabled = False
                 player1Turn = False
-                player2Turn = True
             ElseIf selectLuigi = True Then
-                player1 = 2
+                playerSelected = 2
+                pickLuigi.Enabled = False
                 player1Turn = False
-                player2Turn = True
             ElseIf selectPeach = True Then
-                player1 = 3
+                playerSelected = 3
+                pickPeach.Enabled = False
                 player1Turn = False
-                player2Turn = True
             ElseIf selectYoshi = True Then
-                player1 = 4
+                playerSelected = 4
+                pickYoshi.Enabled = False
                 player1Turn = False
-                player2Turn = True
             End If
-            prompMessage(player1)
-        End If
-
-        player2Turn = False
-
-
-        If player2Turn = True Then
-            If selectMario = True Then
-                player2 = 1
-                player1Turn = False
-                player2Turn = False
-            ElseIf selectLuigi = True Then
-                player1 = 2
-                player1Turn = False
-                player2Turn = False
-            ElseIf selectPeach = True Then
-                player1 = 3
-                player1Turn = False
-                player2Turn = False
-            ElseIf selectYoshi = True Then
-                player1 = 4
-                player1Turn = False
-                player2Turn = False
-            End If
-            prompMessage(player2)
+            prompMessage(playerSelected)
         End If
     End Sub
     Public Sub New()
         InitializeComponent()
-        player1Turn = True
-        player2Turn = False
     End Sub
     Private Sub pickMario_Click(sender As Object, e As EventArgs) Handles pickMario.Click
         selectMario = True
-        pickTurn()
+        pickTurn(True, 1)
     End Sub
 
     Private Sub pickLuigi_Click(sender As Object, e As EventArgs) Handles pickLuigi.Click
         selectLuigi = True
-        pickTurn()
+        pickTurn(True, 2)
     End Sub
 
     Private Sub pickPeach_Click(sender As Object, e As EventArgs) Handles pickPeach.Click
         selectPeach = True
-        pickTurn()
+        pickTurn(True, 3)
     End Sub
 
     Private Sub pickYoshi_Click(sender As Object, e As EventArgs) Handles pickYoshi.Click
         selectYoshi = True
-        pickTurn()
+        pickTurn(True, 4)
     End Sub
 End Class
